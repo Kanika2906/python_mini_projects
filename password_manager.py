e09201b4ba5c2a98022d3a6a082451b7c755132a
Kanika Banga <kanikabanga29@gmail.com>
2026-05-25 11:11:16 +0530

    password manager that saves history

---
+++ password_manager.py
@@ -0,0 +1,53 @@
+import random 
+import string
+passwords = {}
+
+#Load existing password file
+try:
+    with open("passwords.txt","r") as file:
+        for line in file :
+            website, pwd = line.strip().split(":")
+            passwords[website] = pwd
+except:
+    pass
+
+def generate_pwd():
+    chars = string.ascii_letters + string.digits + "&*%$#@!"
+    password = "".join(random.choice(chars) for _ in range(8))
+    return password
+
+while True:
+    print("\n -----PERSONAL PASSWORD GENERATOR -----")
+    print("1. Save Password")
+    print("2. View Password")
+    print("3. Generate Password")
+    print("4. Exit")
+
+    choice = input("enter your choice")
+    if choice == "1":
+        site = input("Enter website")
+        pwd = input("Enter password")
+
+        passwords[site] = pwd
+
+        with open("passwords.txt","a") as file:
+            file.write(f"{site}:{pwd}\n")
+        print("Saved!")
+    
+    elif choice == "2":
+        if not passwords:
+            print("No saved passwords!")
+        else:
+            for site,pwd in passwords.items():
+                print(f"{site}:{pwd}")
+        
+
+    elif choice == "3":
+        print("Generated Password",generate_pwd())
+    
+    elif choice == "4":
+        print("Ok bye")
+        break
+    
+    else:
+        print("Invalid input")
